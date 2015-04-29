5cdd2f2b759f9e757b80fcd1e799eb3fbbbc9638
notanumber11 <denisromasanta@hotmail.com>
2015-04-29 19:13:59 +0200

    Que te jodan

---
+++ src/Others/game.java
@@ -0,0 +1,161 @@
+package Others;
+
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
+import static org.lwjgl.opengl.GL11.glViewport;
+
+public class game {
+//Estupideces
+    /** position of quad */
+    float x = 400, y = 300;
+    /** angle of quad rotation */
+    float rotation = 0;
+
+    //Frames
+    long lastFrame;/** time at last frame */
+    int fps;/** frames per second */
+    long lastFPS;/** last fps time */
+//Enable vsync
+    public static final boolean VSYNC = true;
+    // Width and height of our window
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
+    // Whether to use fullscreen mode
+    public static final boolean FULLSCREEN = false;
+    // Whether our game loop is running
+    protected boolean running = false;
+
+    public void start() {
+        try {
+            Display.setResizable(true); //whether our window is resizable
+            Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
+            Display.setVSyncEnabled(VSYNC); //whether hardware VSync is enabled
+            Display.setFullscreen(FULLSCREEN); //whether fullscreen is enable
+            Display.create();            //Create and show our display
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        create(); // Create our OpenGL context and initialize any resources
+        initGL(); // init OpenGL
+        resize();     // Call this before running to set up our initial size
+        getDelta(); // call once before loop to initialise lastFrame
+        lastFPS = getTime(); // call before loop to initialise fps timer
+        running=true;
+        while (running &&!Display.isCloseRequested()) {
+            if (Display.wasResized())
+                resize();
+            update(getDelta());
+            renderGL();
+            Display.update();
+            Display.sync(60); // cap fps to 60fps
+        }
+        // Dispose any resources and destroy our window
+        dispose();
+        Display.destroy();
+    }
+
+    // Exit our game loop and close the window
+    public void exit() {
+        running = false;
+    }
+
+    public void update(int delta) {
+        // rotate quad
+        rotation += 0.15f * delta;
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) x -= 0.35f * delta;
+        if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) x += 0.35f * delta;
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP)) y -= 0.35f * delta;
+        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) y += 0.35f * delta;
+
+        // keep quad on the screen
+        if (x < 0) x = 0;
+        if (x > 800) x = 800;
+        if (y < 0) y = 0;
+        if (y > 600) y = 600;
+
+        updateFPS(); // update FPS Counter
+    }
+
+    public void renderGL() {
+        // Clear The Screen And The Depth Buffer
+        GL11.glClear(Display.GL_COLOR_BUFFER_BIT | Display.GL_DEPTH_BUFFER_BIT);
+
+        // R,G,B,A Set The Color To Blue One Time Only
+        DisplayMode.glColor3f(0.5f, 0.5f, 1.0f);
+
+        // draw quad
+        Display.glPushMatrix();
+        Display.glTranslatef(x, y, 0);
+        Display.glRotatef(rotation, 0f, 0f, 1f);
+        LWJGLException.glTranslatef(-x, -y, 0);
+
+        System.glBegin(Display.GL_QUADS);
+        Display.glVertex2f(x - 50, y - 50);
+        Display.glVertex2f(x + 50, y - 50);
+        Display.glVertex2f(x + 50, y + 50);
+        Display.glVertex2f(x - 50, y + 50);
+        Keyboard.glEnd();
+        Keyboard.glPopMatrix();
+    }
+
+    // Called to resize our game
+    protected void resize() {
+        Keyboard(0, 0, Keyboard.getWidth(), Keyboard.getHeight());
+    }
+
+
+    //Create objects
+    protected void create() {}
+    //Init opengl
+    public void initGL() {
+        Keyboard.glMatrixMode(Keyboard.GL_PROJECTION);
+        Keyboard.glLoadIdentity();
+        GL11.glOrtho(0, WIDTH, 0, HEIGHT, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+    }
+    // Called to destroy our game upon exiting
+    protected void dispose() {}
+
+    //------------------Time and fps
+    /*** Calculate how many milliseconds have passed
+     * since last frame.*/
+    public int getDelta() {
+        long time = getTime();
+        int delta = (int) (time - lastFrame);
+        lastFrame = time;
+        //System.out.println(delta);
+        return delta;
+    }
+
+    /**Get the accurate system time
+     * @return The system time in milliseconds*/
+    public long getTime() {
+        return (GL11.getTime() * 1000) / GL11.getTimerResolution();
+    }
+
+    /**Calculate the FPS and set it in the title bar*/
+    public void updateFPS() {
+        if (getTime() - lastFPS > 1000) {
+            GL11.setTitle("FPS: " + fps);
+            fps = 0;
+            lastFPS += 1000;
+
+        }
+        fps++;
+    }
+
+    public static void main(GL11[] argv) {
+        game game = new game();
+        game.start();
+    }
+}
