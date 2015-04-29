484951b4de67d921fea9b1c9beb3f6e80d1a53ac
notanumber11 <denisromasanta@hotmail.com>
2015-04-29 19:24:21 +0200

    Que te jodan 2.0

--- src/Others/game.java
+++ src/Others/game.java
@@ -1,5 +1,6 @@
 package Others;
 
+import static org.lwjgl.opengl.GL11.glViewport;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
@@ -88,29 +89,29 @@ public class game {
 
     public void renderGL() {
         // Clear The Screen And The Depth Buffer
-        GL11.glClear(Display.GL_COLOR_BUFFER_BIT | Display.GL_DEPTH_BUFFER_BIT);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
         // R,G,B,A Set The Color To Blue One Time Only
-        DisplayMode.glColor3f(0.5f, 0.5f, 1.0f);
+        GL11.glColor3f(0.5f, 0.5f, 1.0f);
 
         // draw quad
-        Display.glPushMatrix();
-        Display.glTranslatef(x, y, 0);
-        Display.glRotatef(rotation, 0f, 0f, 1f);
-        LWJGLException.glTranslatef(-x, -y, 0);
+        GL11.glPushMatrix();
+        GL11.glTranslatef(x, y, 0);
+        GL11.glRotatef(rotation, 0f, 0f, 1f);
+        GL11.glTranslatef(-x, -y, 0);
 
-        System.glBegin(Display.GL_QUADS);
-        Display.glVertex2f(x - 50, y - 50);
-        Display.glVertex2f(x + 50, y - 50);
-        Display.glVertex2f(x + 50, y + 50);
-        Display.glVertex2f(x - 50, y + 50);
-        Keyboard.glEnd();
-        Keyboard.glPopMatrix();
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(x - 50, y - 50);
+        GL11.glVertex2f(x + 50, y - 50);
+        GL11.glVertex2f(x + 50, y + 50);
+        GL11.glVertex2f(x - 50, y + 50);
+        GL11.glEnd();
+        GL11.glPopMatrix();
     }
 
     // Called to resize our game
     protected void resize() {
-        Keyboard(0, 0, Keyboard.getWidth(), Keyboard.getHeight());
+        glViewport(0, 0, Display.getWidth(), Display.getHeight());
     }
 
 
@@ -118,8 +119,8 @@ public class game {
     protected void create() {}
     //Init opengl
     public void initGL() {
-        Keyboard.glMatrixMode(Keyboard.GL_PROJECTION);
-        Keyboard.glLoadIdentity();
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
         GL11.glOrtho(0, WIDTH, 0, HEIGHT, 1, -1);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
     }
@@ -140,13 +141,13 @@ public class game {
     /**Get the accurate system time
      * @return The system time in milliseconds*/
     public long getTime() {
-        return (GL11.getTime() * 1000) / GL11.getTimerResolution();
+        return (Sys.getTime() * 1000) / Sys.getTimerResolution();
     }
 
     /**Calculate the FPS and set it in the title bar*/
     public void updateFPS() {
         if (getTime() - lastFPS > 1000) {
-            GL11.setTitle("FPS: " + fps);
+            Display.setTitle("FPS: " + fps);
             fps = 0;
             lastFPS += 1000;
 
@@ -154,8 +155,9 @@ public class game {
         fps++;
     }
 
-    public static void main(GL11[] argv) {
+    public static void main(String[] argv) {
         game game = new game();
         game.start();
     }
 }
+
