447022b12cbcf9c76849bc7db9382e148ff2025b
notanumber11 <denisromasanta@hotmail.com>
2015-04-30 13:58:11 +0200

    Prueba 2 Denis

--- src/Game/pruebaCommitLocal.java
+++ src/Game/pruebaCommitLocal.java
@@ -4,4 +4,8 @@ package Game;
  * Created by Denis on 30/04/2015.
  */
 public class pruebaCommitLocal {
+    int precio=0;
+    public void setPrecio(int precio){
+        this.precio=precio;
+    }
 }
