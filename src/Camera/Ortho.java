365f30858949300b854b634306b4cf202bacb2f7
snooze6 <armandonogueirar@gmail.com>
2015-05-01 03:31:26 +0200

    Y porque viene siendo hora de dormir...

--- src/Camera/Ortho.java
+++ src/Camera/Ortho.java
@@ -11,18 +11,17 @@ import static org.lwjgl.opengl.GL11.glLoadIdentity;
 import static org.lwjgl.opengl.GL11.glMatrixMode;
 import static org.lwjgl.opengl.GL11.glRotated;
 import static org.lwjgl.opengl.GL11.glTranslated;
-//import static org.lwjgl.opengl.GL11.*;
-import org.lwjgl.util.glu.GLU;
+import static org.lwjgl.opengl.GL11.glOrtho;
 
-public class Perspective implements ICam {
+public class Ortho implements ICam{
+
 	private float cam_x, cam_y, cam_z;                //Posición de la cámara
-	private float cam_ang_x, cam_ang_y, cam_ang_z;    //Ángulos de la cámara
-	private int angvision;                          //Ángulo de visión (Zoom)
+	private float cam_ang_x, cam_ang_y, cam_ang_z;    //Ángulos de la cámara                //Ángulo de visión (Zoom)
 	private int W_WIDTH, W_HEIGHT;
 	
 	//--------------------------------------------------------------------------
 	
-	public Perspective() {
+	public Ortho() {
 		super();
 		cam_x=0;
 		cam_y=0;
@@ -30,10 +29,9 @@ public class Perspective implements ICam {
 		cam_ang_x=0;
 		cam_ang_y=0;
 		cam_ang_z=0;
-		angvision=45;
 	}
-	public Perspective(float cam_x, float cam_y, float cam_z,
-			float cam_ang_x, float cam_ang_y, float cam_ang_z) {
+	public Ortho(float cam_x, float cam_y, float cam_z, float cam_ang_x,
+			float cam_ang_y, float cam_ang_z) {
 		super();
 		this.cam_x = cam_x;
 		this.cam_y = cam_y;
@@ -41,11 +39,9 @@ public class Perspective implements ICam {
 		this.cam_ang_x = cam_ang_x;
 		this.cam_ang_y = cam_ang_y;
 		this.cam_ang_z = cam_ang_z;
-		this.angvision = 45;
 	}
-	public Perspective(float cam_x, float cam_y, float cam_z,
-			float cam_ang_x, float cam_ang_y, float cam_ang_z,
-			int angvision) {
+	public Ortho(float cam_x, float cam_y, float cam_z, float cam_ang_x,
+			float cam_ang_y, float cam_ang_z, int w_WIDTH, int w_HEIGHT) {
 		super();
 		this.cam_x = cam_x;
 		this.cam_y = cam_y;
@@ -53,44 +49,33 @@ public class Perspective implements ICam {
 		this.cam_ang_x = cam_ang_x;
 		this.cam_ang_y = cam_ang_y;
 		this.cam_ang_z = cam_ang_z;
-		this.angvision = angvision;
-	}
-	public Perspective(float cam_x, float cam_y, float cam_z, float cam_ang_x,
-			float cam_ang_y, float cam_ang_z, int angvision, int w_WIDTH,
-			int w_HEIGHT) {
-		super();
-		this.cam_x = cam_x;
-		this.cam_y = cam_y;
-		this.cam_z = cam_z;
-		this.cam_ang_x = cam_ang_x;
-		this.cam_ang_y = cam_ang_y;
-		this.cam_ang_z = cam_ang_z;
-		this.angvision = angvision;
 		W_WIDTH = w_WIDTH;
 		W_HEIGHT = w_HEIGHT;
 	}
+	
 	//--------------------------------------------------------------------------
 	
 	@Override
 	public void setPosition(float x, float y, float z) {
-		cam_x = x; cam_y = y; cam_z = z;
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void setAngle(float angx, float angy, float angz) {
-		cam_ang_x = angx; cam_ang_y = angy; cam_ang_z = angz;
+		// TODO Auto-generated method stub
 		
 	}
 	@Override
 	public void setZoom(float zoom) {
-		if (!(angvision + zoom < 35 || angvision + zoom > 100)){
-			angvision+=zoom;
-		}
+		// TODO Auto-generated method stub
+		
 	}
-	public void setWindow(int i, int j){
-		W_WIDTH=i;
-		W_HEIGHT=j;
+	@Override
+	public void setWindow(int i, int j) {
+		// TODO Auto-generated method stub
+		
 	}
-	
+
 	//--------------------------------------------------------------------------
 	
 	@Override
@@ -101,9 +86,7 @@ public class Perspective implements ICam {
 
         glMatrixMode(GL_PROJECTION); //La camara
         glLoadIdentity(); // Inicializamos la matriz de la cámara a la identidad
-        //glOrtho(-10*scale, 10*scale, -10, 10, -10, 10);
-        //Perspective Cam
-        GLU.gluPerspective(angvision,(float)W_WIDTH/(float)W_HEIGHT,1,2000);
+        glOrtho(-10, 10, -10, 10, -10, 10);
 
         glRotated(cam_ang_x, 1.0, 0.0, 0.0);
         glRotated(cam_ang_y, 0.0, 1.0, 0.0);
@@ -114,101 +97,110 @@ public class Perspective implements ICam {
 	}
 	
 	//--------------------------------------------------------------------------
-	
+
 	@Override
 	public void moveUp(float v) {
-		cam_y=cam_y+5;
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void moveDown(float v) {
-		cam_y=cam_y-5;
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void moveStraight(float v) {
-        cam_x+=Math.sin(Math.toRadians(cam_ang_y))*v;
-        cam_z-=Math.cos(Math.toRadians(cam_ang_y))*v;
-        cam_y-=Math.sin(Math.toRadians(cam_ang_x))*v;		
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void moveBack(float v) {
-        cam_x-=Math.sin(Math.toRadians(cam_ang_y))*v;
-        cam_z+=Math.cos(Math.toRadians(cam_ang_y))*v;
-        cam_y+=Math.sin(Math.toRadians(cam_ang_x))*v;		
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void moveRight(float v) {
-        cam_x+=Math.cos(Math.toRadians(cam_ang_y))*v;
-        cam_z+=Math.sin(Math.toRadians(cam_ang_y))*v;
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void moveLeft(float v) {
-        cam_x-=Math.cos(Math.toRadians(cam_ang_y))*v;
-        cam_z-=Math.sin(Math.toRadians(cam_ang_y))*v;	
+		// TODO Auto-generated method stub
+		
 	}
 	
 	//--------------------------------------------------------------------------
 	
-	private float updateang(float ang){
-		if (ang>360){
-			return ang-360;
-		} else {
-			if (ang<-360) {
-				return ang+360;
-			} else {
-				return ang;
-			}
-		}
-	}
-	@Override
-	public void lookUp(float ang) {
-		cam_ang_x=updateang(cam_ang_x-ang);
-	}
-	@Override
-	public void lookDown(float ang) {
-		cam_ang_x=updateang(cam_ang_x+ang);
-	}
 	@Override
 	public void lookRight(float ang) {
-		cam_ang_y=updateang(cam_ang_y+ang);
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void lookLeft(float ang) {
-		cam_ang_y=updateang(cam_ang_y-ang);
-	}
+		// TODO Auto-generated method stub
 		
+	}
+	@Override
+	public void lookUp(float ang) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void lookDown(float ang) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	//--------------------------------------------------------------------------
 	
 	@Override
 	public void morezoom() {
-		setZoom(1);
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void lesszoom() {
-		setZoom(-1);
+		// TODO Auto-generated method stub
 		
 	}
-	
+
 	//--------------------------------------------------------------------------
 	
+	@Override
 	public float getCam_x() {
-		return cam_x;
-	}
-	public float getCam_y() {
-		return cam_y;
-	}
-	public float getCam_z() {
-		return cam_z;
-	}
-	public float getCam_ang_x() {
-		return cam_ang_x;
-	}
-	public float getCam_ang_y() {
-		return cam_ang_y;
-	}
-	public float getCam_ang_z() {
-		return cam_ang_z;
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
-	
-	
+	@Override
+	public float getCam_y() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getCam_z() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getCam_ang_x() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getCam_ang_y() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getCam_ang_z() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
